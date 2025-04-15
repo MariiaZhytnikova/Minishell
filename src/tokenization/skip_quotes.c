d0175c1c01a07e0ebacd3ed7b65ac3412a71da2b
Mariia Zhytnikova <allusio@gmail.com>
2025-04-15 11:26:48 +0300

    bug in skip quotes

--- src/tokenization/skip_quotes.c
+++ src/tokenization/skip_quotes.c
@@ -6,56 +6,73 @@
 /*   By: mzhitnik <mzhitnik@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/10 09:45:50 by mzhitnik          #+#    #+#             */
-/*   Updated: 2025/04/10 13:36:15 by mzhitnik         ###   ########.fr       */
+/*   Updated: 2025/04/15 11:06:32 by mzhitnik         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-char	*skip_quotes(const char *arg)
+void	skip_quotes(const char *arg, t_temp *thing)
+{
+	while (arg[thing->i])
+	{
+		if (arg[thing->i] == '\'')
+		{
+			thing->i++;
+			while (arg[thing->i] && arg[thing->i] != '\'')
+				thing->temp[thing->j++] = arg[thing->i++];
+			thing->i++;
+		}
+		else if (arg[thing->i] == '\"')
+		{
+			thing->i++;
+			while (arg[thing->i] && arg[thing->i] != '\"')
+				thing->temp[thing->j++] = arg[thing->i++];
+			thing->i++;
+		}
+		else
+			thing->temp[thing->j++] = arg[thing->i++];
+	}
+}
+
+int	check_in(char **input)
+{
+	t_temp	thing;
+	int		id;
+
+	id = 0;
+	while (input[id])
+	{
+		ft_memset(thing.temp, 0, MAX_PROMT);
+		thing.i = 0;
+		thing.j = 0;
+		if (ft_strchr(input[id], '\'') || ft_strchr(input[id], '\"'))
+		{
+			skip_quotes(input[thing.i], &thing);
+			free(input[id]);
+			input[id] = ft_strdup(thing.temp);
+			if (!input[id])
+				return (error_msg("ft_strdup Fail", NULL, NULL, NULL), -1);
+		}
+		id++;
+	}
+	return (1);
+}
+
+int	check_last(char *input)
 {
 	t_temp	thing;
 
 	ft_memset(thing.temp, 0, MAX_PROMT);
 	thing.i = 0;
 	thing.j = 0;
-	while (arg[thing.i])
+	if (ft_strchr(input, '\'') || ft_strchr(input, '\"'))
 	{
-		if (arg[thing.i] == '\'')
-		{
-			thing.i++;
-			while (arg[thing.i] && arg[thing.i] != '\'')
-				thing.temp[thing.j++] = arg[thing.i++];
-			thing.i++;
-		}
-		else if (arg[thing.i] == '\"')
-		{
-			thing.i++;
-			while (arg[thing.i] && arg[thing.i] != '\"')
-				thing.temp[thing.j++] = arg[thing.i++];
-			thing.i++;
-		}
-		else
-			thing.temp[thing.j++] = arg[thing.i++];
-	}
-	return (ft_strdup(thing.temp));
-}
-
-int	check_in(char **input)
-{
-	int		i;
-	char	*buffer;
-
-	i = 0;
-	buffer = NULL;
-	while (input[i])
-	{
-		if (ft_strchr(input[i], '\'') || ft_strchr(input[i], '\"'))
-		{
-			free(input[i]);
-			input[i] = skip_quotes(input[i]);
-		}
-		i++;
+		skip_quotes(input, &thing);
+		free(input);
+		input = ft_strdup(thing.temp);
+		if (!input)
+			return (error_msg("ft_strdup Fail", NULL, NULL, NULL), -1);
 	}
 	return (1);
 }
@@ -95,6 +112,10 @@ int	skip(t_session *session)
 	while (id < session->count->cmd_nb)
 	{
 		if (check_case(session, session->cmds[id], id) < 0)
+			return (-1);
+		if (check_last(session->cmds[id]->last_in->name) < 0)
+			return (-1);
+		if (check_last(session->cmds[id]->last_out->name) < 0)
 			return (-1);
 		id++;
 	}
