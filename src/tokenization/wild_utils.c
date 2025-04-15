03ca56a37f8eb9b169592febe4610795c4b0513d
Mariia Zhytnikova <allusio@gmail.com>
2025-04-15 11:27:45 +0300

    use of skip_quotes changed

--- src/tokenization/wild_utils.c
+++ src/tokenization/wild_utils.c
@@ -6,7 +6,7 @@
 /*   By: mzhitnik <mzhitnik@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/26 10:46:24 by mzhitnik          #+#    #+#             */
-/*   Updated: 2025/04/10 14:46:58 by mzhitnik         ###   ########.fr       */
+/*   Updated: 2025/04/14 16:38:32 by mzhitnik         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -86,11 +86,15 @@ bool	is_in_quotes(const char *str)
 
 int	create_new(t_list **args, t_list *new, char *str)
 {
-	new = ft_lstnew(skip_quotes(str));
+	t_temp	thing;
+
+	ft_memset(thing.temp, 0, MAX_PROMT);
+	thing.i = 0;
+	thing.j = 0;
+	skip_quotes(str, &thing);
+	new = ft_lstnew(ft_strdup(thing.temp));
 	if (!new)
 		return (error_msg("ft_lstnew skip_quotes_no_ast", NULL, NULL, NULL), -1);
 	ft_lstadd_back(args, new);
 	return (1);
 }
-
-// https://github.com/osorosia/minishell/blob/main/.test/ok/wildcard.txt
